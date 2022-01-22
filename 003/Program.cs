bee74b934fbc8f453309ad1ec9896e90da39f3b0
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-01-22 14:32:30 +0300

    003 complete

---
+++ 003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+if(number > 5)
+{
+    Console.WriteLine("Это выходной день.");
+}
+else
+{
+    Console.WriteLine("Это будний день.");
+}
+if(number >= 8)
+{
+    Console.WriteLine("Вы ввели некоректный день недели.");
+}
