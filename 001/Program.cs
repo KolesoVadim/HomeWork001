01eb3daa6f6d3b0eef44ffc4732e2878c0b16c45
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-01-22 13:00:47 +0300

    001 complete

---
+++ 001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Проверить кратно ли число 7 и 23.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Число кратно 7 и 23");
+}
+else 
+{
+    Console.WriteLine("Число не кратно 7 и 23");
+}
+
+// || - логическое "ИЛИ"
+// && - логическое "И"
+// ! - логическое "НЕ"
